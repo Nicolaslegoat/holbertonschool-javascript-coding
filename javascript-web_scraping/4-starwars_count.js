b729f1ccc96772e6b49e5edb77815fcc0738efa3
Nicolaslegoat <n.mancino03@gmail.com>
2024-01-25 01:23:54 -0800

    syntaxe peoples

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,13 +6,13 @@ const apiURL = process.argv[2];
 const characterID = 18;
 
 request(apiURL, (err, response, body) => {
-    if (err) {
-        console.error(err);
-    } else {
-        const moviedata = JSON.parse(body).results;
-        const wedgesantilles = moviedata.filter((movie) =>
-        movie.characters.some((character) => character.includes(`/peoples/${characterID}/`))
-        );
-        console.log(wedgesantilles.length);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    const moviedata = JSON.parse(body).results;
+    const wedgeantilles = moviedata.filter((movie) =>
+      movie.characters.some((character) => character.includes(`/people/${characterID}/`))
+    );
+    console.log(wedgeantilles.length);
+  }
 });

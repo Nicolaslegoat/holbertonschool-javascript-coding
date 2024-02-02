0637a4768186ab811fc0a56ae796478fb6a3c2d4
Nicolaslegoat <n.mancino03@gmail.com>
2024-02-02 09:00:50 -0800

    create http server using http module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'test/plain'});
+    res.end('Hello Holberton School!');
+});
+app.listen,(1245, () => {
+    console.log('Server running at http= //localhost:1245/');
+});
+module.exports = app;
